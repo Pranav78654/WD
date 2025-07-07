043a96d34b3895095c67d9a2d3b516ea9406acf9
Pb78654 <pb78654@gmail.com>
2025-07-07 15:32:17 +0530

    work in class
    asysnc await ,  fetch on html

--- 5.js
+++ 5.js
@@ -5,7 +5,22 @@ async function fetchdata(){
     try{
     const fetchdata  =await fetch("https://dummyjson.com/products");
     const data = await fetchdata.json();
-    console.log(data);
+    const simpleproducts = data.products.map(product =>({
+        title: product.title,
+        desc: product.description,
+        price: product.price
+    }));
+
+    const show = document.getElementById("products");
+     simpleproducts.forEach(product => {
+      show.insertAdjacentHTML('beforeend', `
+        <div style="border:1px solid #ccc; padding:10px; margin:10px;">
+          <h2>${product.title}</h2>
+          <p>${product.desc}</p>
+          <strong>$${product.price}</strong>
+        </div>
+      `);
+    });
     }
     catch(err){
         console.log(err.message)

611dcf2c41172e31bd8703f200792629c584c102
Pb78654 <pb78654@gmail.com>
2025-07-08 15:14:30 +0530

    fomated

--- assignment/5.js
+++ assignment/5.js
@@ -1,18 +1,18 @@
 // async await with fetch api
 
 
-async function fetchdata(){
-    try{
-    const fetchdata  =await fetch("https://dummyjson.com/products");
+async function fetchdata() {
+  try {
+    const fetchdata = await fetch("https://dummyjson.com/products");
     const data = await fetchdata.json();
-    const simpleproducts = data.products.map(product =>({
-        title: product.title,
-        desc: product.description,
-        price: product.price
+    const simpleproducts = data.products.map(product => ({
+      title: product.title,
+      desc: product.description,
+      price: product.price
     }));
 
     const show = document.getElementById("products");
-     simpleproducts.forEach(product => {
+    simpleproducts.forEach(product => {
       show.insertAdjacentHTML('beforeend', `
         <div style="border:1px solid #ccc; padding:10px; margin:10px;">
           <h2>${product.title}</h2>
@@ -21,10 +21,10 @@ async function fetchdata(){
         </div>
       `);
     });
-    }
-    catch(err){
-        console.log(err.message)
-    }
+  }
+  catch (err) {
+    console.log(err.message)
+  }
 }
 
 fetchdata();
